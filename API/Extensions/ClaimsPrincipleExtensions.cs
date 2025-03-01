945080c6098d41987a8732bae96dbee2c599aeb4
MRHM22 <hdezmartin22@gmail.com>
2025-02-28 18:13:01 -0600

    cambios de la sesion 13

--- API/Extensions/ClaimsPrincipleExtensions.cs
+++ API/Extensions/ClaimsPrincipleExtensions.cs
@@ -10,4 +10,11 @@ public static class ClaimsPrincipleExtensions
             ?? throw new System.Exception("Cannot get username from token");
         return username;
     }
+
+    public static int GetUserId(this ClaimsPrincipal user)
+    {
+        var userId = int.Parse( user.FindFirstValue(ClaimTypes.NameIdentifier)
+            ?? throw new System.Exception("Cannot get username from token" ));
+        return userId;
+    }
 }
